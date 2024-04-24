65393692ed76dc39d9c6e144d6a5323d3c3fead4
malithie <malithim@wso2.com>
2024-04-24 18:56:42 +0530

    updated sdk version and removed ballerina dependencies file.

--- petcare-sample/b2c/web-app/petdesk/web/react/config.js
+++ petcare-sample/b2c/web-app/petdesk/web/react/config.js
@@ -1,6 +1,6 @@
 window.config = {
-    baseUrl: "https://localhost:9443",
-    clientID: "<CONFIGURED_SPA_CLIENT_ID>",
+    baseUrl: "https://dev.api.asgardeo.io/t/wso2con2024iam",
+    clientID: "eV3M8K6YtfW6_BBGi89PLvke_xwa",
     signInRedirectURL: "http://localhost:5173",
     signOutRedirectURL: "http://localhost:5173",
     resourceServerURL: "http://localhost:9090",
