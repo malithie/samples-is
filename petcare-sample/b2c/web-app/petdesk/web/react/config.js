6edf0dddb3b295f8b9f153525405992aa0149c8a
malithie <malithim@wso2.com>
2024-04-25 12:40:37 +0530

    add a config to add myaccount app url as it is different from base path in asgardeo.

--- petcare-sample/b2c/web-app/petdesk/web/react/config.js
+++ petcare-sample/b2c/web-app/petdesk/web/react/config.js
@@ -1,8 +1,9 @@
 window.config = {
-    baseUrl: "https://dev.api.asgardeo.io/t/wso2con2024iam",
-    clientID: "eV3M8K6YtfW6_BBGi89PLvke_xwa",
+    baseUrl: "",
+    clientID: "",
     signInRedirectURL: "http://localhost:5173",
     signOutRedirectURL: "http://localhost:5173",
+    myAccountAppURL: "",
     resourceServerURL: "http://localhost:9090",
     scope: ["openid", "email", "profile"]
  };
